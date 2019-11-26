bc62ea976f5e33ac9d8fad56239f3e022f8c91a7
Mert <mert@connected2.me>
2019-11-25 17:20:02 -0800

    image text check and detect label APIs added.

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
 from chalice import Chalice
+from botocore.exceptions import ClientError
+import boto3
 
 app = Chalice(app_name='AwsRekognition-API')
+app.debug = False
+
+client = boto3.client('rekognition')
+s3 = boto3.resource('s3')
+
+
+@app.route('/detect_labels_in_image')
+def detect_labels_in_image():
+    s3_image = app.current_request.query_params.get('s3_image')
+    try:
+        response = client.detect_labels(
+            Image={
+                'S3Object': {
+                    'Bucket': 'rekognitiontrialbucketnudephotos',
+                    'Name': s3_image,
+                }
+            }
+        )
+        return response
+    except ClientError as e:
+        return {"status": "Error: %s" % e.response['Error']['Message']}
+
+
+@app.route('/detect_texts_in_image')
+def detect_texts_in_image():
+    s3_image = app.current_request.query_params.get('s3_image')
+    try:
+        response = client.detect_text(
+            Image={
+                'S3Object': {
+                    'Bucket': 'rekognitiontrialbucketc2m',
+                    'Name': s3_image,
+                }
+            }
+        )
+        return response
+    except ClientError as e:
+        return {"status": "Error: %s" % e.response['Error']['Message']}
 
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
