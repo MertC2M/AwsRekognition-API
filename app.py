0b58bdd9316b8f8b17d8e6ed6882167f1677609e
Mert <mert@connected2.me>
2019-11-25 19:17:25 -0800

    moderation API added and outputs arranged.

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ s3 = boto3.resource('s3')
 
 @app.route('/detect_labels_in_image')
 def detect_labels_in_image():
+    all_labels = []
     s3_image = app.current_request.query_params.get('s3_image')
     try:
         response = client.detect_labels(
@@ -19,9 +20,12 @@ def detect_labels_in_image():
                     'Bucket': 'rekognitiontrialbucketnudephotos',
                     'Name': s3_image,
                 }
-            }
+            },
+            MaxLabels=10
         )
-        return response
+        for label in response['Labels']:
+            all_labels.append('Label: ' + '%s' % label['Name'] + ' Confidence: ' + '%s' % label['Confidence'])
+        return all_labels
     except ClientError as e:
         return {"status": "Error: %s" % e.response['Error']['Message']}
 
@@ -29,6 +33,7 @@ def detect_labels_in_image():
 @app.route('/detect_texts_in_image')
 def detect_texts_in_image():
     s3_image = app.current_request.query_params.get('s3_image')
+    all_texts = []
     try:
         response = client.detect_text(
             Image={
@@ -38,7 +43,33 @@ def detect_texts_in_image():
                 }
             }
         )
-        return response
+        for text in response['TextDetections']:
+            all_texts.append(
+                'DetectedText: ' + '%s' % text['DetectedText'] + ' Confidence: %' + '{:.2f}'.format(text['Confidence']))
+        return all_texts
+    except ClientError as e:
+        return {"status": "Error: %s" % e.response['Error']['Message']}
+
+
+@app.route('/detect_moderation_label_in_image')
+def detect_moderation_label_in_image():
+    s3_image = app.current_request.query_params.get('s3_image')
+    all_moderation_labels = []
+    try:
+        response = client.detect_moderation_labels(
+            Image={
+                'S3Object': {
+                    'Bucket': 'rekognitiontrialbucketnudephotos',
+                    'Name': s3_image,
+                }
+            },
+            MinConfidence=70,
+        )
+        for label in response['ModerationLabels']:
+            all_moderation_labels.append(
+                'Name: ' + '%s' % label['Name'] + ' ParentName: ' + '%s' % label['ParentName']
+                + ' Confidence: %' + '{:.2f}'.format(label['Confidence']))
+        return all_moderation_labels
     except ClientError as e:
         return {"status": "Error: %s" % e.response['Error']['Message']}
 
